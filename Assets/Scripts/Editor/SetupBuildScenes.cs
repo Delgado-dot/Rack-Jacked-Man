35f8e8b1d9f351b85d0b9e03e68651fcb861b0dd
J0H4Ns9 <johansaico563@gmail.com>
2026-07-16 19:08:41 -0500

    Actualizado

---
+++ Assets/Scripts/Editor/SetupBuildScenes.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using System.Collections.Generic;
+using System.IO;
+
+/// <summary>
+/// SetupBuildScenes - Agrega todas las escenas al Build Settings.
+/// Ejecutar desde: Rack-Jacked-Man > Setup Build Scenes
+/// </summary>
+public class SetupBuildScenes
+{
+    [MenuItem("Rack-Jacked-Man/Setup Build Scenes")]
+    public static void Execute()
+    {
+        string scenesPath = "Assets/Scenes";
+        string[] sceneFiles = Directory.GetFiles(scenesPath, "*.unity");
+        List<EditorBuildSettingsScene> scenes = new List<EditorBuildSettingsScene>();
+
+        // Orden deseado
+        string[] orden =
+        {
+            "MenuPrincipal",
+            "Nivel_1",
+            "SubCable01_Copy",
+            "Menu Victoria",
+            "Menu GameOver",
+            "Puzzle_Cables",
+            "Puzzle_Dispatcher",
+            "Puzzle_Nave",
+            "Puzzle_PatchCore",
+            "Puzzle_Trafico",
+            "Nivel_2",
+            "PlayerTest",
+            "SampleScene"
+        };
+
+        HashSet<string> added = new HashSet<string>();
+
+        // Agregar en orden primero
+        foreach (string nombre in orden)
+        {
+            string path = scenesPath + "/" + nombre + ".unity";
+            if (File.Exists(path))
+            {
+                scenes.Add(new EditorBuildSettingsScene(path, true));
+                added.Add(nombre);
+                Debug.Log("[SetupBuildScenes] Agregada: " + nombre);
+            }
+        }
+
+        // Agregar las que queden (si alguna no estaba en la lista)
+        foreach (string file in sceneFiles)
+        {
+            string nombre = Path.GetFileNameWithoutExtension(file);
+            if (!added.Contains(nombre))
+            {
+                scenes.Add(new EditorBuildSettingsScene(file, true));
+                added.Add(nombre);
+                Debug.Log("[SetupBuildScenes] Agregada (extra): " + nombre);
+            }
+        }
+
+        EditorBuildSettings.scenes = scenes.ToArray();
+        Debug.Log("[SetupBuildScenes] Total escenas en Build Settings: " + scenes.Count);
+        EditorUtility.DisplayDialog("Setup Build Scenes",
+            scenes.Count + " escenas agregadas al Build Settings.", "OK");
+    }
+}
---
+++ Assets/Scripts/Managers/MenuSelector.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class MenuSelector : MonoBehaviour
+{
+    void Awake()
+    {
+        Time.timeScale = 1f;
+    }
+
+    void Start()
+    {
+        DisableChildButtonsOnText();
+        ConnectButton("Guardar y Reintentar", RestartLevel);
+        ConnectButton("Reintentar", RestartLevel);
+        ConnectButton("Volver al Menu", LoadMainMenu);
+        ConnectButton("Salir", QuitGame);
+    }
+
+    private void DisableChildButtonsOnText()
+    {
+        Canvas canvas = GetComponent<Canvas>();
+        if (canvas == null) return;
+
+        Button[] allButtons = canvas.GetComponentsInChildren<Button>(true);
+        foreach (Button btn in allButtons)
+        {
+            if (btn.GetComponent<TextMeshProUGUI>() != null)
+            {
+                btn.enabled = false;
+                Debug.Log("[MenuSelector] Button desactivado en TextMeshPro: " + btn.gameObject.name);
+            }
+        }
+    }
+
+    private void ConnectButton(string buttonName, UnityEngine.Events.UnityAction action)
+    {
+        GameObject obj = GameObject.Find(buttonName);
+        if (obj == null) return;
+
+        Button button = obj.GetComponent<Button>();
+        if (button != null)
+        {
+            button.onClick.AddListener(action);
+            Debug.Log("[MenuSelector] Boton conectado: " + buttonName);
+        }
+    }
+
+    private void RestartLevel()
+    {
+        Time.timeScale = 1f;
+
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.ResetState();
+        }
+
+        PlayerHealth.ResetForNewScene();
+        SceneManager.LoadScene("Nivel_1");
+    }
+
+    private void LoadMainMenu()
+    {
+        Time.timeScale = 1f;
+
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.LoadMainMenu();
+        }
+        else
+        {
+            SceneManager.LoadScene("MenuPrincipal");
+        }
+    }
+
+    private void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
